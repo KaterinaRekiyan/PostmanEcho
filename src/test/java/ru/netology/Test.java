4eb6eba4811969d5504625069d56cbbdf5a45243
Ekaterina <Katerina.rekiyan@yandex.ru>
2023-01-13 00:17:24 +0300

    fist

---
+++ src/test/java/ru/netology/Test.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import static io.restassured.RestAssured.given;
+public class Test {
+
+    @Test
+    void returnBody() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("name")
+        ;
+    }
+}
+
